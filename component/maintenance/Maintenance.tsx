dc9e484237fd9b8420ad701d2937489e03bcd1e5
SojitraJenil <jenilsojitra4@gmail.com>
2024-05-09 18:53:40 +0530

    AS

--- component/maintenance/Maintenance.tsx
+++ component/maintenance/Maintenance.tsx
@@ -1,14 +1,21 @@
-import Image from 'next/image'
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 function Maintenance() {
     return (
-        <div>
-            <center>
-                <Image height={800} width={800} src={require("../../image/download.svg")} alt="" />
-                <p className='text-3xl font-bold'>We are under Maintenance</p>
-            </center>
+        <div className="maintenance-container">
+            <div className="maintenance-content">
+                <p className="maintenance-text my-3">We are under Maintenance</p>
+                <Image
+                    src={require("../../image/download.svg")}
+                    alt=""
+                    width={600}
+                    height={600}
+                    style={{ animation: 'pulse 2s infinite alternate' }}
+                />
+            </div>
         </div>
-    )
+    );
 }
-export default Maintenance
+
+export default Maintenance;
