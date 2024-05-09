68b8ba73eac573c99569f2a152259a469202ca06
SojitraJenil <jenilsojitra4@gmail.com>
2024-05-09 15:41:57 +0530

    changes

--- component/chat/ChatWhatsapp.tsx
+++ component/chat/ChatWhatsapp.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { FaArrowLeft, FaMicrophone, FaUserCircle } from "react-icons/fa";
+import { FaMicrophone, FaUserCircle } from "react-icons/fa";
 import { RotatingLines } from "react-loader-spinner";
 import { IoSend } from "react-icons/io5";
 import { MdArrowBack, MdCall, MdOutlineEmojiEmotions } from "react-icons/md";
@@ -246,7 +246,7 @@ function Chat() {
               ?
               <div className="flex justify-between items-center border-b border-black pb-2 p-2 rounded-md mb-4 border-bottom text-white bg-[#035F51] sticky sm:px-4 lg:px-8 xl:px-2">
                 <div className="flex items-center  w-full sm:w-auto">
-                  <MdArrowBack className="text-2xl me-2 my-3" onClick={() => { SetSearch(false) }} />
+                  <MdArrowBack className="me-3 my-3 text-2xl mr-1 mt-3 " onClick={() => { SetSearch(false) }} />
                   <div className="relative w-full sm:w-auto md:w-[290px] lg:w-[290px]">
                     <input type="text" className="bg-transparent border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 p-2.5 sm:w-[290px] md:w-[290px] lg:w-[290px]" placeholder="Search Messages..." required />
                   </div>
@@ -257,7 +257,7 @@ function Chat() {
               <div className="flex justify-between items-center border-b border-black pb-2 p-2 rounded-md mb-4 border-bottom text-white bg-[#035F52] sticky sm:px-4 lg:px-8 xl:px-2 z-50">
                 <div className="flex ps-1 ">
                   <MdArrowBack
-                    className="text-2xl mr-1 mt-3"
+                    className="text-2xl mr-1 me-3 mt-3"
                     onClick={() => { router.push("/users") }}
                   />
                   <FaUserCircle className="w-8 h-8 mr-1 mt-2 " />
